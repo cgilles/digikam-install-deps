02b2d76df75908e084191b95bb2f5c6ba44a1d10
Gilles Caulier <caulier.gilles@gmail.com>
2020-06-21 21:20:03 +0200

    comments deps

--- preparehost-mageia.sh
+++ preparehost-mageia.sh
@@ -12,101 +12,98 @@
 # Packages for base dependencies and Qt5.
 
 urpmi --auto \
-      wget \
-      tar \
-      bzip2 \
-      gettext \
-      git \
-      subversion \
-      libtool \
-      which \
-      fuse \
-      automake \
-      cmake \
-      gcc-c++ \
-      patch \
-      libxcb \
-      libxcb-devel \
-      xcb-util-keysyms-devel \
-      xcb-util-devel \
-      xkeyboard-config \
-      xscreensaver \
-      gperf \
-      ruby \
-      bison \
-      flex \
-      zlib-devel \
-      expat-devel \
-      fuse-devel \
-      glibc-devel \
-      mysql-devel \
-      eigen3-devel \
-      cppunit-devel \
-      libstdc++-devel \
-      libstdc++-static-devel \
-      lcms2-devel \
-      glibc-devel \
-      lib64udev-devel \
-      sqlite-devel \
-      xz-devel \
-      lz4-devel \
-      inotify-tools-devel \
-      cups-devel \
-      openal-soft-devel \
-      fontconfig-devel \
-      freetype-devel \
-      patchelf \
-      dpkg \
-      ninja \
-      python \
-      ruby \
-      ruby-devel \
-      sqlite3-devel \
-      ffmpeg-devel \
-      boost-devel \
-      gphoto2-devel \
-      sane-backends \
-      jasper-devel \
-      lib64exif-devel \
-      lib64xslt-devel \
-      lib64drm-devel \
-      lib64icu-devel \
-      lib64proxy-devel \
-      lib64ical-devel \
-      lib64cap-devel \
-      lib64xml2-devel \
-      lib64nss-devel \
-      lib64xkbcommon-devel \
-      lib64sane1-devel \
-      lib64xcb-util1 \
-      lib64xi-devel \
-      lib64xtst-devel \
-      lib64xrandr-devel \
-      lib64xcursor-devel \
-      lib64xcomposite-devel \
-      lib64xrender-devel \
-      lib64mesagl1-devel \
-      lib64mesaglu1-devel \
-      lib64mesaegl1-devel \
-      lib64mesaegl1 \
-      lib64ltdl-devel \
-      lib64glib2.0-devel \
-      lib64usb1.0-devel \
-      lib64jpeg-devel \
-      lib64png-devel \
-      lib64tiff-devel \
-      lib64lqr-devel \
-      lib64fftw-devel \
-      lib64curl-devel \
-      lib64magick-devel \
-      lib64wayland-devel
+      wget \                        # For GMicQt.
+      tar \                         # AppImage legacy ?
+      bzip2 \                       # AppImage legacy ?
+      gettext \                     # i18n.
+      git \                         # source code is hosted on git server.
+      subversion \                  # translations are hosted on svn server.
+      libtool \                     # AppImage legacy ?
+      which \                       # AppImage legacy ?
+      fuse \                        # AppImage legacy ?
+      automake \                    # AppImage legacy ?
+      cmake \                       # To compile source code.
+      gcc-c++ \                     # To compile source code.
+      patch \                       # To compile source code.
+      libxcb \                      # X11 requirement for QtBase.
+      libxcb-devel \                # X11 requirement for QtBase.
+      xcb-util-keysyms-devel \      # X11 requirement for QtBase.
+      xcb-util-devel \              # X11 requirement for QtBase.
+      xkeyboard-config \            # X11 requirement for QtBase.
+      xscreensaver \                # X11 requirement for QtBase.
+      gperf \                       # To compile Qt.
+      zlib-devel \                  # To compile Qt.
+      ruby \                        # translations extractions scripts.
+      bison \                       # Panorama tool.
+      flex \                        # Panorama tool.
+      expat-devel \                 # For Exiv2 and Adobe DSK
+      fuse-devel \                  # AppImage legacy ?
+      glibc-devel \                 # To compile Qt.
+      mysql-devel \                 # To compile Qt.
+      eigen3-devel \                # Refocus tool.
+      cppunit-devel \               # AppImage legacy ?
+      libstdc++-devel \             # For QtWebEngine
+      libstdc++-static-devel \      # For QtWebEngine
+      lcms2-devel \                 # Color management support.
+      glibc-devel \                 # To compile Qt.
+      lib64udev-devel \             # AppImage legacy ?
+      xz-devel \                    # To compile Qt.
+      lz4-devel \                   # To compile Qt.
+      inotify-tools-devel \         # To compile Qt.
+      cups-devel \                  # To compile Qt.
+      openal-soft-devel \           # For QtAv
+      fontconfig-devel \            # To compile Qt.
+      freetype-devel \              # To compile Qt.
+      patchelf \                    # AppImage legacy ?
+      dpkg \                        # AppImage legacy ?
+      ninja \                       # For QtWebEngine
+      python \                      # For QtWebEngine
+      ruby-devel \                  # For QtWebEngine
+      sqlite3-devel \               # To compile Qt.
+      ffmpeg-devel \                # For QtAv
+      boost-devel \                 # DK deps.
+      gphoto2-devel \               # DK deps.
+      sane-backends \               # DK deps.
+      jasper-devel \                # DK deps.
+      lib64exif-devel \             # AppImage legacy ?
+      lib64xslt-devel \             # DK deps.
+      lib64drm-devel \              # To compile Qt.
+      lib64icu-devel \              # To compile Qt.
+      lib64proxy-devel \            # To compile Qt.
+      lib64ical-devel \             # DK deps.
+      lib64cap-devel \              # To compile Qt.
+      lib64xml2-devel \             # DK deps.
+      lib64nss-devel \              # To compile Qt.
+      lib64xkbcommon-devel \        # To compile Qt.
+      lib64sane1-devel \            # KF5 deps.
+      lib64xcb-util1 \              # For QtWebEngine
+      lib64xi-devel \               # For QtWebEngine
+      lib64xtst-devel \             # For QtWebEngine
+      lib64xrandr-devel \           # For QtWebEngine
+      lib64xcursor-devel \          # For QtWebEngine
+      lib64xcomposite-devel \       # For QtWebEngine
+      lib64xrender-devel \          # For QtWebEngine
+      lib64mesagl1-devel \          # For QtWebEngine
+      lib64mesaglu1-devel \         # For QtWebEngine
+      lib64mesaegl1-devel \         # For QtWebEngine
+      lib64mesaegl1 \               # For QtWebEngine
+      lib64ltdl-devel \             # For QtWebEngine
+      lib64glib2.0-devel \          # For QtWebEngine
+      lib64usb1.0-devel \           # For Gphoto2 support
+      lib64jpeg-devel \             # DK deps.
+      lib64png-devel \              # DK deps.
+      lib64tiff-devel \             # DK deps.
+      lib64lqr-devel \              # DK deps.
+      lib64fftw-devel \             # QMicQt.
+      lib64curl-devel \             # QMicQt.
+      lib64magick-devel \           # DK deps.
+      lib64wayland-devel            # To Compile Qt.
 
 # Remove system based devel package to prevent conflict with new one.
 
 urpme --auto --force lib64qt5core5-devel \
                      lib64openssl-devel \
                      lib64exiv2-devel \
-                     opencv-devel \
                      lensfun-devel \
                      lib64qtav-devel \
                      marble-devel \
