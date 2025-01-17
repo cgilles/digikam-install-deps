5f60699770f013ac9fef28f9ac6877311b1523e2
Gilles Caulier <caulier.gilles@gmail.com>
2025-01-17 14:21:55 +0100

    update

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@
 #
 # Script to configure project based on CMake for Linux.
 #
-# Copyright (c) 2013-2024, Gilles Caulier, <caulier dot gilles at gmail dot com>
+# Copyright (c) 2013-2025, Gilles Caulier, <caulier dot gilles at gmail dot com>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
