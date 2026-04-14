26ec36ba90de82ebb3ffb2c9e59e4a16694e7e17
Gilles Caulier <caulier.gilles@gmail.com>
2026-04-14 10:09:19 +0200

    bump version

--- config.sh
+++ config.sh
@@ -34,11 +34,11 @@ INSTALL_DIR="/opt/qt6"
 
 # KDE KF6 frameworks version.
 # See official release here: https://download.kde.org/stable/frameworks/
-DK_KDE_VERSION="v6.24.0"
+DK_KDE_VERSION="v6.25.0"
 
 # KDE Plasma version.
 # See official release here: https://download.kde.org/stable/plasma/
-DK_KP_VERSION="v6.6.2"
+DK_KP_VERSION="v6.6.4"
 
 # KDE Application version.
 # See official release here: https://download.kde.org/stable/release-service/
